2e524432b5096934d35bb4ba04cde1cc2911682b
smile-e3 <2278215957@qq.com>
2024-04-03 00:53:01 +0800

    update:openssl heartbleed env_build.sh

--- practice/openssl_heartbleed/env_build.sh
+++ practice/openssl_heartbleed/env_build.sh
@@ -9,4 +9,5 @@ LOG_LEVEL_ALL
 # copy openssl source code to here
 INFO "Copy Openssl Source Code To Here!"
 tar -zxvf ../../goals/openssl-1.0.1f.tar.gz -C .
+mkdir crash_output
 INFO "Let's start fuzzing"
